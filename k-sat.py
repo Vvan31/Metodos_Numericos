3a05f4b3ffd02ae8d696be2fc7d6abc09167996d
Viviana Osorio <71792220+Vvan31@users.noreply.github.com>
2022-04-28 18:05:25 -0500

    k-sat complete

--- k-sat.py
+++ k-sat.py
@@ -1,6 +1,7 @@
 # input: a formula in  -CNF with variables
 # Guess an initial assignment a E {0,1}n,
     # uniformly at random
+    
 # Repeat 3n times:
     # If the formula is satisfied by the actual 
     # assignment: stop and accept
@@ -11,6 +12,7 @@
     # Pick one of the <=k literals in the clause
     # at random and flip its value
     # in the current assignment
+from operator import contains
 import random
 
 def manage_file(file): 
@@ -34,24 +36,63 @@ def manage_file(file):
             clause = []
             for n in clause_var:
                 if n != '0':
-                    clause.append(n)
+                    clause.append(int(n))
 
             clauses.append(clause)
     f.close()
     return n_binary_variables,n_clauses,clauses
 
 # Generate a random bit. 
-def generate_random(): 
-    return (random.randint(0,1))
+def generate_random(min, max): 
+    return (random.randint(min,max))
+
+def generate_input(n_binary_variables):
+    input = []
+    for n in range (0,int(n_binary_variables)):
+        input.append(random.choice([-1,1]))
+    return input
 
 # Guess an initial assignment at random. 
-def initial_guess(arr):
-    return 0
+def guess(input,clauses):
+    llave = -1
+    bad_clauses = []
+    good_clauses = []
+    for clause in  clauses:
+        llave += 1
+        result = 0
+        for variable in clause: 
+            var_result = input[abs(variable)-1] * variable
+            if var_result < 0: 
+                result +=1
+
+        if result > 2: 
+            bad_clauses.append(clause)
+        else:
+            good_clauses.append(clause)
+    return bad_clauses,good_clauses
+
+
+def flip_variable(bad_clause, input): 
+    n_toflip = generate_random(0,2)
+    variable_toflip = bad_clause[n_toflip]
+    new_input = input
+    new_input[abs(variable_toflip)-1] *= -1 
+    return new_input
+
 
 def main(): 
     n_binary_variables,n_clauses,clauses = manage_file("Instance3SATExample.txt")
-    print(n_binary_variables )
-    print( n_clauses) 
-    print(clauses)
-    
+    input= generate_input(n_binary_variables)
+    bad_clauses, good_clauses = guess(input,clauses)
+    n = 0
+    while n < 3*int(n_binary_variables):
+         n +=1
+         print(f"bad_clauses:{bad_clauses}")
+         print(f"good_clauses:{good_clauses}")
+         if len(bad_clauses) < 0: # not true
+            break
+         else: 
+            new_input = flip_variable(bad_clauses[0], input)
+            bad_clauses,good_clauses = guess(new_input,clauses)
+
 main()
